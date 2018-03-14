80a4e218ef0e3d0c243f7f747a7c3426b1117167
yakeworld <gushiedu@gmail.com>
2018-03-14 18:33:53 +0800

    Create sci-hub_automatic_link.js

---
+++ sci-hub_automatic_link.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        sci-hub automatic link
+// @namespace https://github.com/yakeworld/sci-hub/
+// @version     1.0
+// @description link DOI to sci-hub.la
+// @author      yakeworld
+// @match       *://*/*
+// @grant       none
+// @run-at      document-idle
+
+// ==/UserScript==
+window.addEventListener('load', function() {
+   var els = document.getElementsByTagName("A");
+   for(var i = 0, l = els.length; i < l; i++) {
+      var el = els[i];
+      el.href = el.href.replace(/^((http(s|):\/\/(www\.|))|)(dx.|)doi\.org\//gim, 'http://sci-hub.la/');
+   }
+}, false);
